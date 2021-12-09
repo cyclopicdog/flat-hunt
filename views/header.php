319778cfb5ff19805c4e3a6df3a8db5a615c983b
cyclopicdog <jonathan_doig@hotmail.com>
2021-12-09 15:51:18 +0100

    starter files

---
+++ views/header.php
@@ -0,0 +1,10 @@
+  <header class="header">
+    <h1 class="header__page-name">flat hunt</h1>
+    <h2 class="header__sub-heading">...a record of c's</h2>
+    <div class="header__login">
+
+    <?php
+    include ('login.php');
+    ?>
+    </div>
+  </header>
---
+++ views/login.php
@@ -0,0 +1,6 @@
+<form action="" method="post">
+  <label for="username">username</label>
+  <input type="text" name="username">
+  <label for="password"></label>
+  <input type="password" name="password">
+</form>
